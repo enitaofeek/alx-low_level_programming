79848ae668657348254ccd2525452b389c0cdbe0
enitaofeek <enitaofeek@gmail.com>
2023-03-23 11:53:56 -0700

    function nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * add - The function is created to add two valuees
+ * @n1: The first number to be computed
+ * @n2: The second number to be computed
+ * Return: Interger value
+ */
+
+int add(int n1, int n2)
+
+{
+
+	return (n1 + n2);
+
+}
+
